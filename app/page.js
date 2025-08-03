6ca963c80c2f61e623607d0963906ac5b4e83c4a
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-08-03 14:49:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ const page = () => {
   return (
     <>
       <div className='h-screen flex flex-col justify-center items-center '>
-        <h1> Mohanavenkatesh</h1>
+        <h1> Mohan</h1>
         <p>Full-Stack Developer</p>
         <p>
           Building clean, scalable apps. Exploring AI tools to work smarter. Always learning, always coding.
         </p>
-        <button>Hire Me</button>
+        <Button>Hire ME</Button>
 
       </div>
 
