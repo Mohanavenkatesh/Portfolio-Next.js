fea6b5b27b21e7a6ac5d7ab267c4cebd4640b6ca
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-08-14 22:47:04 +0530

    Add theme-based Next.js logos to portfolio page
    Introduced dark and light theme Next.js SVG logos and updated the portfolio page to display the appropriate logo based on the user's theme. Also added a 'My Skills' section header to improve content structure.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Analytics } from '@vercel/analytics/react';
 import React from "react";
+import Image from "next/image";
+
 
 const page = () => {
   return (
@@ -9,7 +11,10 @@ const page = () => {
         <div className="flex flex-col ">
           <h1 className="mb-8  text-2xl font-semibold">My Portfolio</h1>
           <p>I’m Mohan, a passionate frontend developer with a knack for building clean, responsive, and user-friendly web applications. Skilled in HTML, CSS, JavaScript, and modern frameworks like React and Next.js, I focus on crafting engaging UI experiences with attention to detail and performance. My goal is to blend aesthetics with functionality, creating interfaces that users love to interact with.</p>
+          <h1 className="my-8  text-2xl font-semibold">My Skills</h1>
         </div>
+        <Image src="/nextjs-dark-theme.svg" width={50} height={50} className="hidden dark:block" alt="Next.js Dark Theme Logo" priority />
+        <Image src="/nextjs-light-theme.svg" width={50} height={50} className="block dark:hidden" alt="Next.js Light Theme Logo" priority />
         <Analytics />
       </div>
     </>
